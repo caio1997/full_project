21efaf76a1146d495e172aa022cde8dc05dd7b24
Caio Marinho <caio_mcoelho@hotmail.com>
2020-06-16 20:10:32 -0300

    Projeto criado

---
+++ src/main/java/com/myhome/full_project/FullProjectApplication.java
@@ -0,0 +1,13 @@
+package com.myhome.full_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myhome/full_project/FullProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myhome.full_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
