0454eeed67dfe39706bd7f000b7bbbf5048710a6
Caio Marinho <caio_mcoelho@hotmail.com>
2020-06-16 21:31:28 -0300

    Criado entity Categoria e controller Categoria

---
+++ src/main/java/com/myhome/full_project/controllers/CategoriaController.java
@@ -0,0 +1,11 @@
+package com.myhome.full_project.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
+
+
+}
