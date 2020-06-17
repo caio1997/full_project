51aceae098d119e92427d1ed34d4b068cc93c2de
Caio Marinho <caio_mcoelho@hotmail.com>
2020-06-16 21:45:55 -0300

    Mapeamento da classe Categoria com Spring Data JPA

--- src/main/java/com/myhome/full_project/entities/Categoria.java
+++ src/main/java/com/myhome/full_project/entities/Categoria.java
@@ -2,9 +2,17 @@ package com.myhome.full_project.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
 	private Long id;
 	private String nome;
 	
