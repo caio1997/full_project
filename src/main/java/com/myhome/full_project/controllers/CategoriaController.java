ed7bf64d78c2506a0ad593bdef3d4450ce24bb8a
Caio Marinho <caio_mcoelho@hotmail.com>
2020-06-21 17:33:20 -0300

    Paginação da classe Categoria

--- src/main/java/com/myhome/full_project/controllers/CategoriaController.java
+++ src/main/java/com/myhome/full_project/controllers/CategoriaController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -58,6 +60,17 @@ public class CategoriaController {
 	public ResponseEntity<List<CategoriaDTO>> findAll(){
 		List<Categoria> list = categoriaService.findAll();
 		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok(listDTO);		
+	}
+	
+	@GetMapping(value = "/page")
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(name = "page", defaultValue = "0") Integer page,
+			@RequestParam(name = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(name = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(name = "direction", defaultValue = "ASC") String direction){
+		Page<Categoria> list = categoriaService.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listDTO = list.map(obj -> new CategoriaDTO(obj));
 		return ResponseEntity.ok(listDTO);
 				
 	}
--- src/main/java/com/myhome/full_project/services/CategoriaService.java
+++ src/main/java/com/myhome/full_project/services/CategoriaService.java
@@ -5,12 +5,16 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.myhome.full_project.entities.Categoria;
 import com.myhome.full_project.repositories.CategoriaRepository;
 import com.myhome.full_project.services.exceptions.DataIntegrityException;
 import com.myhome.full_project.services.exceptions.IdIsNotfound;
+
 
 @Service
 public class CategoriaService {
@@ -46,6 +50,11 @@ public class CategoriaService {
 	public List<Categoria> findAll(){
 		List<Categoria> list = categoriaRepository.findAll();
 		return list;
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return categoriaRepository.findAll(pageRequest);
 	}
 	
 }
